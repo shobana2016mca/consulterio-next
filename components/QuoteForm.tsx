6cb4f87162b4d72cd2924ecb3ac2cde82df83026
Abhijit <59195447+Abhijit47@users.noreply.github.com>
2024-05-22 21:02:54 +0530

    Added email functionality

--- components/QuoteForm.tsx
+++ components/QuoteForm.tsx
@@ -18,18 +18,18 @@ export default function QuoteForm() {
     getValues,
     reset,
   } = useForm<QuoteFormInputsType>({
-    defaultValues: {
-      enquirerName: 'Jhon Doe',
-      companyName: 'XYZ Company',
-      email: 'jhondoe123@gmail.com',
-      phoneNo: '9999911111',
-      location: 'Chennai',
-      jobRole: 'Project manager',
-      monthlySalary: 14000,
-      // annualSalary: 0,
-      peopleCount: '',
-      about: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
+    // defaultValues: {
+    //   enquirerName: 'Jhon Doe',
+    //   companyName: 'XYZ Company',
+    //   email: 'jhondoe123@gmail.com',
+    //   phoneNo: '9999911111',
+    //   location: 'Chennai',
+    //   jobRole: 'Project manager',
+    //   monthlySalary: 14000,
+    //   // annualSalary: 0,
+    //   peopleCount: '',
+    //   about: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    // },
   });
   // const [instance, updateInstance] = usePDF({ document: '' });
   const [formData, setFormData] = useState<QuoteFormInputsType>();
